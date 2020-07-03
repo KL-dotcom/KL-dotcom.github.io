3a359f1b06c3c67aa2f0af30325379b4ccced499
Krissy Little <kristenlittle1992@gmail.com>
2020-07-03 17:21:54 +0100

    the header is animated

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,23 @@
 function init() {
 
 
+  const title = document.querySelector('h1')
+  const titleText = '<h1>Hello World!</h1>'
+  const titleSpeed = 100
+  let i = 0
+
+  function writingText() {
+
+    if (i <= titleText.length) {
+      title.innerHTML += titleText.charAt(i)
+      i++
+      setTimeout(writingText, titleSpeed)
+    }
+
+  }
 
 
-
-
-
-
+  writingText()
 
 
 
